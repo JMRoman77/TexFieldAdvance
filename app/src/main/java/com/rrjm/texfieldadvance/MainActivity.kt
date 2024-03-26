b558ec45ae3aa07b95afb7fae21939da62c56af4
JMROM\jmrom <77rrjm@gmail.com>
2024-03-26 20:05:59 +0100

    FirstCommit Empty project

--- app/src/main/java/com/rrjm/texfieldadvance/MainActivity.kt
+++ app/src/main/java/com/rrjm/texfieldadvance/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,25 +21,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyTexFieldAdvance()
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PreviewInitial(){
+    MyTexFieldAdvance()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TexFieldAdvanceTheme {
-        Greeting("Android")
-    }
+fun MyTexFieldAdvance() {
+
 }
+
